6c0948a49073ce7bc9adf84cb68ac2c31072b6bb
Aunmag <aunmag@gmail.com>
2021-03-17 02:34:08 +0500

    Reworked input sending

--- src/systems/net/input_send.rs
+++ src/systems/net/input_send.rs
@@ -3,78 +3,101 @@ use crate::components::ActorActions;
 use crate::components::Player;
 use crate::resources::Message;
 use crate::resources::NetResource;
+use crate::utils::DurationExt;
+use amethyst::core::timing::Time;
 use amethyst::core::transform::Transform;
 use amethyst::derive::SystemDesc;
-use amethyst::ecs::prelude::Join;
-use amethyst::ecs::prelude::ReadStorage;
-use amethyst::ecs::prelude::System;
-use amethyst::ecs::prelude::SystemData;
+use amethyst::ecs::Join;
+use amethyst::ecs::Read;
+use amethyst::ecs::ReadStorage;
+use amethyst::ecs::System;
+use amethyst::ecs::SystemData;
 use amethyst::ecs::WriteExpect;
 use std::time::Duration;
-use std::time::Instant;
 
-#[allow(clippy::integer_division)]
-pub const DIRECTION_SEND_INTERVAL_ACTIVE: Duration = Duration::from_millis(1000 / 60);
+/// 25 Hz
+const DIRECTION_SEND_INTERVAL_ACTIVE: Duration = Duration::from_millis(40);
 
-#[allow(clippy::integer_division)]
-pub const DIRECTION_SEND_INTERVAL_PASSIVE: Duration = Duration::from_millis(1000 / 10);
+/// 10 Hz
+const DIRECTION_SEND_INTERVAL_PASSIVE: Duration = Duration::from_millis(100);
 
 #[derive(SystemDesc)]
 pub struct InputSendSystem {
-    // TODO: It may spam input messages if there will be more than one player, find a way to fix it
-    last_actions: ActorActions,
-    last_direction: f32,
-    last_direction_send: Instant,
+    previous: InputSend,
+}
+
+struct InputSend {
+    time: Duration,
+    actions: ActorActions,
+    direction: f32,
 }
 
 impl InputSendSystem {
     pub fn new() -> Self {
         return Self {
-            last_actions: ActorActions::empty(),
-            last_direction: 0.0,
-            last_direction_send: Instant::now(),
+            previous: InputSend {
+                time: Duration::from_millis(0),
+                actions: ActorActions::empty(),
+                direction: 0.0,
+            },
         };
     }
 }
 
 impl<'a> System<'a> for InputSendSystem {
     type SystemData = (
+        Read<'a, Time>,
         ReadStorage<'a, Actor>,
         ReadStorage<'a, Player>,
         ReadStorage<'a, Transform>,
         WriteExpect<'a, NetResource>,
     );
 
-    fn run(&mut self, (actors, players, transforms, mut net): Self::SystemData) {
-        #[allow(clippy::float_cmp)]
+    fn run(&mut self, (time, actors, players, transforms, mut net): Self::SystemData) {
+        #[allow(clippy::never_loop)]
         for (_, actor, transform) in (&players, &actors, &transforms).join() {
-            let direction = transform.euler_angles().2;
+            let current = InputSend {
+                time: time.absolute_real_time(),
+                actions: actor.actions,
+                direction: transform.euler_angles().2,
+            };
 
-            if self.last_actions != actor.actions {
-                net.send_to_all(Message::ClientInput {
+            let message;
+
+            #[allow(clippy::float_cmp, clippy::if_not_else)]
+            if current.actions != self.previous.actions {
+                message = Some(Message::ClientInput {
                     id: 0,
-                    actions: actor.actions.bits(),
-                    direction,
+                    actions: current.actions.bits(),
+                    direction: current.direction,
                 });
+            } else if current.direction != self.previous.direction {
+                let interval;
 
-                self.last_actions = actor.actions;
-                self.last_direction = direction;
-                self.last_direction_send = Instant::now();
-            } else if self.last_direction != direction {
-                let direction_sync_interval;
-
-                if actor.actions.is_empty() {
-                    direction_sync_interval = DIRECTION_SEND_INTERVAL_PASSIVE;
+                if current.actions.is_empty() {
+                    interval = DIRECTION_SEND_INTERVAL_PASSIVE;
                 } else {
-                    direction_sync_interval = DIRECTION_SEND_INTERVAL_ACTIVE;
+                    interval = DIRECTION_SEND_INTERVAL_ACTIVE;
                 }
 
-                if self.last_direction_send.elapsed() > direction_sync_interval {
-                    net.send_to_all(Message::ClientInputDirection { id: 0, direction });
-                    self.last_direction = direction;
-                    self.last_direction_send = Instant::now();
+                if current.time.sub_safely(self.previous.time) > interval {
+                    message = Some(Message::ClientInputDirection {
+                        id: 0,
+                        direction: current.direction,
+                    });
+                } else {
+                    message = None;
                 }
+            } else {
+                message = None;
             }
+
+            if let Some(message) = message {
+                net.send_to_all(message);
+                self.previous = current;
+            }
+
+            break; // since there should be only one player
         }
     }
 }
