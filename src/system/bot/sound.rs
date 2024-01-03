8a1a4864c9354da6051906312ae099225e72d1b2
Aunmag <aunmag@gmail.com>
2024-01-04 02:27:48 +0500

    Fixed zombies sounds played by humans

--- src/system/bot/sound.rs
+++ src/system/bot/sound.rs
@@ -1,4 +1,8 @@
-use crate::{component::Bot, model::AudioPlay, resource::AudioTracker};
+use crate::{
+    component::{Actor, ActorKind, Bot},
+    model::AudioPlay,
+    resource::AudioTracker,
+};
 use bevy::{
     ecs::system::Res,
     math::Vec3Swizzles,
@@ -11,13 +15,17 @@ use std::{ops::Range, time::Duration};
 const INTERVAL: Range<Duration> = Duration::from_secs(2)..Duration::from_secs(30);
 
 pub fn sound(
-    mut bots: Query<(&mut Bot, &Transform)>,
+    mut bots: Query<(&mut Bot, &Actor, &Transform)>,
     mut audio: ResMut<AudioTracker>,
     time: Res<Time>,
 ) {
     let time = time.elapsed();
 
-    for (mut bot, transform) in bots.iter_mut() {
+    for (mut bot, actor, transform) in bots.iter_mut() {
+        if actor.config.kind == ActorKind::Human {
+            continue;
+        }
+
         if !bot
             .voice_timer
             .next_if_ready(time, || rand::thread_rng().gen_range(INTERVAL))
@@ -26,7 +34,7 @@ pub fn sound(
         }
 
         audio.queue(AudioPlay {
-            path: "actors/zombie/misc".into(),
+            path: format!("{}/misc", actor.config.kind.get_assets_path()).into(),
             volume: 0.7,
             source: Some(transform.translation.xy()),
             ..AudioPlay::DEFAULT
