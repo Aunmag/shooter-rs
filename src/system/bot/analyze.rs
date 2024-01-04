13192045efa23ac332dee4e3712f24b3e435addd
Aunmag <aunmag@gmail.com>
2024-01-05 04:15:52 +0500

    Optimized bots

--- src/system/bot/analyze.rs
+++ src/system/bot/analyze.rs
@@ -1,6 +1,6 @@
 use crate::{
-    component::{Actor, Bot, Inertia},
-    util::{ext::RngExt, math::find_meet_point},
+    component::{Actor, Bot},
+    util::ext::RngExt,
 };
 use bevy::{
     ecs::query::BatchingStrategy,
@@ -14,15 +14,15 @@ const TEAMMATES_MAX: usize = 8;
 const UPDATE_INTERVAL: Duration = Duration::from_millis(1500);
 
 pub fn analyze(
-    mut bots: Query<(&mut Bot, Entity, &Actor, &Transform, &Inertia)>,
-    actors: Query<(Entity, &Actor, &Transform, &Inertia)>,
+    mut bots: Query<(&mut Bot, Entity, &Actor, &Transform)>,
+    actors: Query<(Entity, &Actor, &Transform)>,
     time: Res<Time>,
 ) {
     let time = time.elapsed();
 
     bots.par_iter_mut()
         .batching_strategy(BatchingStrategy::fixed(32))
-        .for_each(|(mut bot, e1, a1, t1, i1)| {
+        .for_each(|(mut bot, e1, a1, t1)| {
             if !bot.update_timer.is_ready_or_disabled(time) {
                 return;
             }
@@ -34,52 +34,72 @@ pub fn analyze(
 
             let p1 = t1.translation.xy();
 
-            let mut teammates = Vec::<(Entity, f32)>::with_capacity(TEAMMATES_MAX);
             let mut enemy_distance = f32::MAX;
+            let mut teammates = Teammates::new();
 
-            for (e2, a2, t2, i2) in actors.iter() {
+            for (e2, a2, t2) in actors.iter() {
                 if e1 == e2 {
                     continue;
                 }
 
-                let p2 = t2.translation.xy();
+                let distance = p1.distance_squared(t2.translation.truncate());
 
                 if a1.config.kind == a2.config.kind {
-                    let distance = (p1 - p2).length_squared();
                     let distance_max = bot.spread * 2.0;
 
                     if distance < distance_max * distance_max {
-                        if teammates.len() < TEAMMATES_MAX {
-                            teammates.push((e2, distance));
-                        } else {
-                            replace_farthest(&mut teammates, e2, distance);
-                        }
+                        teammates.try_add(e2, distance);
                     }
-                } else {
-                    let position_meet = find_meet_point(p1, i1.velocity.length(), p2, i2.velocity);
-                    let distance = (p1 - position_meet).length_squared();
-
-                    if distance < enemy_distance {
-                        bot.enemy = Some(e2);
-                        enemy_distance = distance;
-                    }
+                } else if distance < enemy_distance {
+                    bot.enemy = Some(e2);
+                    enemy_distance = distance;
                 }
             }
 
-            bot.teammates = teammates.iter().map(|t| t.0).collect();
+            bot.teammates = teammates.teammates;
         });
 }
 
-fn replace_farthest(teammates: &mut [(Entity, f32)], entity: Entity, distance_new: f32) {
-    let mut farthest: Option<(usize, f32)> = None;
+struct Teammates {
+    teammates: Vec<Entity>,
+    distances: Vec<f32>,
+    furthest_i: usize,
+}
 
-    for (i, (_, distance)) in teammates.iter().enumerate() {
-        if *distance > distance_new && farthest.map_or(true, |f| *distance > f.1) {
-            farthest = Some((i, *distance));
+impl Teammates {
+    fn new() -> Self {
+        return Self {
+            teammates: Vec::with_capacity(TEAMMATES_MAX),
+            distances: Vec::with_capacity(TEAMMATES_MAX),
+            furthest_i: 0,
+        };
+    }
+
+    fn try_add(&mut self, teammate: Entity, distance: f32) {
+        let distance_max = self.distances.get(self.furthest_i).copied().unwrap_or(0.0);
+
+        if self.teammates.len() < TEAMMATES_MAX {
+            self.teammates.push(teammate);
+            self.distances.push(distance);
+
+            if distance > distance_max {
+                self.furthest_i = self.teammates.len() - 1;
+            }
+        } else if distance < distance_max {
+            self.teammates[self.furthest_i] = teammate;
+            self.distances[self.furthest_i] = distance;
+            self.update_furthest();
         }
     }
 
-    if let Some((i, _)) = farthest {
-        teammates[i] = (entity, distance_new);
+    fn update_furthest(&mut self) {
+        let mut distance_max = 0.0;
+
+        for (i, distance) in self.distances.iter().enumerate() {
+            if *distance > distance_max {
+                self.furthest_i = i;
+                distance_max = *distance;
+            }
+        }
     }
 }
